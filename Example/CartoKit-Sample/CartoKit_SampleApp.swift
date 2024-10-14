ae962769821f154ed8a1288f30bbf3bad797c859
Michel-André Chirita <michel.chirita@gmail.com>
2024-10-14 20:50:15 +0200

    first commit

---
+++ Example/CartoKit-Sample/CartoKit_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CartoKit_SampleApp.swift
+//  CartoKit-Sample
+//
+//  Created by Michel-Andre Chirita on 14/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct CartoKit_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
